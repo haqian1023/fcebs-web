a326f29a104bb6f690fb5af75878d825eb438005
Hujhb <445192015@qq.com>
2019-06-05 19:15:09 +0800

    相关配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+
+//引用axios，并设置基础URL作为后端服务api地址
+let axios = require('axios');
+axios.defaults.baseURL = "http://127.0.0.1:8081/fcebs";
+
+//将API方法绑定到全局
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    publicPath: '/fcebs-web',
+    devServer: {
+        host:'127.0.0.1',
+        port:'8080'        
+    }
+};
